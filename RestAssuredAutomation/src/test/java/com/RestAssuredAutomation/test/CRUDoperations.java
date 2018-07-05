17a04f62cebe2cc1c905af373833402391a7464d
sravyaavege@gmail.com <sravyaavege@gmail.com>
2018-07-05 17:37:26 +0530

    1st commit

---
+++ RestAssuredAutomation/src/test/java/com/RestAssuredAutomation/test/CRUDoperations.java
@@ -0,0 +1,101 @@
+package com.RestAssuredAutomation.test;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+
+
+
+/*import io.restassured.RestAssured;
+import io.restassured.response.Response;*/
+
+public class CRUDoperations {
+
+/*	@Test
+	public void getTest()
+	{
+		Response resp=RestAssured.get("http://localhost:3000/posts");
+		//RestAssured is a class & get is a Mtd
+		//we are sending a request that req is get mtd and we are getting the resp and store it into resp obj
+		int stcode=resp.getStatusCode();
+		System.out.println(stcode);
+		//we are checking the status code for that req
+		String respbody=resp.asString();
+		System.out.println(respbody);
+		//asString is to get the response body
+		Assert.assertEquals(stcode, 200);
+	}*/
+	
+	/*@Test
+	public void postTest()
+	{
+		RequestSpecification req = RestAssured.given();
+		//we are using given bcz we hav to give certain specifications like body we need json format we hav to select
+		req.header("content-Type","application/json");
+		JSONObject jobj=new JSONObject();
+		jobj.put("id", "15");
+		jobj.put("title", "API");
+		jobj.put("author","sravs");
+		req.body(jobj.toString());
+		//this body is for to send the body in postmantool in raw
+		Response resp = req.post("http://localhost:3000/posts");
+		int stcode=resp.getStatusCode();
+		System.out.println(stcode);
+		String respbody=resp.asString();
+		System.out.println(respbody);
+		Assert.assertEquals(stcode, 201);
+	}*/
+	
+	
+	/*@Test
+	public void putTest()
+	{
+		RequestSpecification req = RestAssured.given();
+		//we are using given bcz we hav to give certain specifications like body we need json format we hav to select
+		req.header("content-Type","application/json");
+		JSONObject jobj=new JSONObject();
+		jobj.put("id", "15");
+		jobj.put("title", "API");
+		jobj.put("date", "5 july");
+		req.body(jobj.toString());
+		//this body is for to send the body in postmantool in raw
+		Response resp = req.put("http://localhost:3000/posts/15");
+		int stcode=resp.getStatusCode();
+		System.out.println(stcode);
+		String respbody=resp.asString();
+		System.out.println(respbody);
+		Assert.assertEquals(stcode, 200);
+	}*/
+	
+	/*@Test
+	public void patchTest()
+	{
+		RequestSpecification req = RestAssured.given();
+		//we are using given bcz we hav to give certain specifications like body we need json format we hav to select
+		req.header("content-Type","application/json");
+		JSONObject jobj=new JSONObject();
+		jobj.put("date", "31 july");
+		req.body(jobj.toString());
+		//this body is for to send the body in postmantool in raw
+		Response resp = req.patch("http://localhost:3000/posts/15");
+		int stcode=resp.getStatusCode();
+		System.out.println(stcode);
+		String respbody=resp.asString();
+		System.out.println(respbody);
+		Assert.assertEquals(stcode, 200);
+	}*/
+	
+	@Test
+	public void deleteTest()
+	{
+		RequestSpecification req = RestAssured.given();
+		//we are using given bcz we hav to give certain specifications like body we need json format we hav to select
+		Response resp = req.delete("http://localhost:3000/posts/11");
+		int stcode=resp.getStatusCode();
+		System.out.println(stcode);
+		Assert.assertEquals(stcode, 200);
+	}
+}
